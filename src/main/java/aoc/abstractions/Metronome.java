fa30c6a72e92cbf9300fbba5a995178aa6d84d82
jimmy dano <dano.jimmy@gmail.com>
2013-10-24 10:55:54 +0200

    metronome fonctions

--- src/main/java/aoc/abstractions/Metronome.java
+++ src/main/java/aoc/abstractions/Metronome.java
@@ -1,9 +1,44 @@
 package aoc.abstractions;
 
+import java.rmi.activation.ActivationGroupDesc.CommandEnvironment;
+
+import aoc.interfaces.patterncommand.ICommand;
+
 public abstract class Metronome {
 
+	private long tempo;
+	private int tempoParMesure;
+	private boolean etatMarche;
+	
 	public Metronome() {
 		
 	}
 	
+	public long getTempo() {
+		return tempo;
+	}
+	
+	public int getTempoParMesure() {
+		return tempoParMesure;
+	}
+	
+	public void setTempo(long t) {
+		tempo = t;
+	}
+	
+	public void setTempsParMesure(int tps){
+		tempoParMesure = tps;
+	}
+	
+	public void setCmd(String str, ICommand c) {
+		
+	}
+	
+	public void setEtatMarche(boolean b){
+		etatMarche = true;
+	}
+	
+	public void setEtatStop(boolean b){
+		 setEtatMarche = false;
+	}
 }
